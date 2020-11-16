7b4d860630c9c1514118523aad1f41fa7dc07473
chandra655 <chandra.penugonda@unacademy.com>
2020-11-16 17:31:17 +0530

    fix audio issue for  letter

--- javascript drumstick/script.js
+++ javascript drumstick/script.js
@@ -1,18 +1,19 @@
+// play audio if available
 function playAudio(e) {
-  const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
-  const key = document.querySelector(`.key[data-key='${e.keyCode}']`);
+  const keyCode = e.keyCode;
+  const audio = document.querySelector(`audio[data-key='${keyCode}']`);
+  const key = document.querySelector(`.key[data-key='${keyCode}']`);
   if (!audio) return;
   audio.currentTime = 0;
   audio.play();
   key.classList.add("playing");
 }
-
 window.addEventListener("keydown", playAudio);
 
+// remove transition
 function removeTransition(e) {
   if (e.propertyName !== "transform") return;
   e.target.classList.remove("playing");
 }
-
 const keys = document.querySelector(".keys");
 keys.addEventListener("transitionend", removeTransition);
